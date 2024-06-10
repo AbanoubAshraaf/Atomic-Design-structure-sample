e1e3071b745e0425718c20846c7a462a6dcdefb4
AbanoubAshraaf <abanoubashrf25@gmail.com>
2024-06-10 12:51:38 +0200

    feat(EmailStep): add validation to EmailStep component

--- src/components/molecules/EmailStep/index.tsx
+++ src/components/molecules/EmailStep/index.tsx
@@ -10,6 +10,17 @@ const EmailStep: React.FC<EmailStepProps> = ({ onClick }) => {
   const [email, setEmail] = useState<string>("");
   const [error, setError] = React.useState<string>("");
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validateAndProceed = () => {
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address.");
+    } else {
+      setError("")
+      onClick("email", email);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4 w-full">
       <InputField
@@ -19,7 +30,7 @@ const EmailStep: React.FC<EmailStepProps> = ({ onClick }) => {
         error={error}
         onChange={(value) => setEmail(value)}
       />
-      <Button text="Next" onClick={() => onClick("email", email)} />
+      <Button text="Next" onClick={validateAndProceed} />
     </div>
   );
 };
