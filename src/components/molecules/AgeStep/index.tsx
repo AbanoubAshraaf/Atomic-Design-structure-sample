3882430578208cc1a20b9be1d8db9372f19e88d6
AbanoubAshraaf <abanoubashrf25@gmail.com>
2024-06-10 13:34:02 +0200

    feat(AgeStep): add validation to AgeStep component

--- src/components/molecules/AgeStep/index.tsx
+++ src/components/molecules/AgeStep/index.tsx
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import InputField from "@/components/atoms/InputField";
 import Button from "@/components/atoms/Button";
 import { StepProps } from "@/components/types";
+import { isValidAge } from "@/components/utils";
 
 const AgeStep: React.FC<StepProps> = ({ onClick }) => {
   const [age, setAge] = useState(0);
   const [error, setError] = React.useState<string>("");
+
+  const validateAndProceed = () => {
+    if (!isValidAge(age)) {
+      setError("Please enter a valid positive integer age.");
+    } else {
+      setError("");
+      onClick("age", age);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4 w-full">
@@ -16,7 +26,7 @@ const AgeStep: React.FC<StepProps> = ({ onClick }) => {
         error={error}
         onChange={(value) => setAge(Number(value))}
       />
-      <Button text="Buy Now" onClick={() => onClick("age", age)} />
+      <Button text="Buy Now" onClick={validateAndProceed} />
     </div>
   );
 };
