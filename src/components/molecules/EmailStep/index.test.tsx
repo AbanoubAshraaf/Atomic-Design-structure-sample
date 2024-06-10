6b59efd93d3eab6f582a80d38c3c3a0ddc311780
AbanoubAshraaf <abanoubashrf25@gmail.com>
2024-06-10 13:45:34 +0200

    test(EmailStep): edit EmailStep test file

--- src/components/molecules/EmailStep/index.test.tsx
+++ src/components/molecules/EmailStep/index.test.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import EmailStep from "@/components/molecules/EmailStep";
 import { mockValidUserData } from "@/components/test/mocks";
 import { createSnapShot } from "@/components/test/utils";
 
 describe("<EmailStep/> Component tests", () => {
   test("render email input properly", () => {
-    const { container } = render(<EmailStep onClick={jest.fn()} />);
-    expect(screen.getByTestId("email")).toBeInTheDocument();
+    const { container, getByTestId } = render(<EmailStep onClick={jest.fn()} />);
+    expect(getByTestId("email")).toBeInTheDocument();
 
     createSnapShot(container)
   });
 
   test("display an error for invalid email", () => {
-    const { container } = render(<EmailStep onClick={jest.fn()} />);
-    const input = screen.getByTestId('email');
+    const { container, getByTestId, getByText } = render(<EmailStep onClick={jest.fn()} />);
+    const input = getByTestId('email');
     fireEvent.change(input, { target: { value: 'invalid-email' } });
-    fireEvent.click(screen.getByText("Next"));
-    expect(screen.getByText("Please enter a valid email address.")).toBeInTheDocument();
+    fireEvent.click(getByText("Next"));
+    expect(getByText("Please enter a valid email address.")).toBeInTheDocument();
 
     createSnapShot(container)
   });
 
   test("call callback with email value on button click", () => {
     const mockCallback = jest.fn();
-    render(<EmailStep onClick={mockCallback} />);
+    const { getByTestId, getByText } = render(<EmailStep onClick={mockCallback} />);
 
-    fireEvent.change(screen.getByTestId("email"), { target: { value: mockValidUserData.email } });
-    fireEvent.click(screen.getByText("Next"));
+    fireEvent.change(getByTestId("email"), { target: { value: mockValidUserData.email } });
+    fireEvent.click(getByText("Next"));
     expect(mockCallback).toHaveBeenCalledWith("email", "test@getsafe.com");
   });
 });
